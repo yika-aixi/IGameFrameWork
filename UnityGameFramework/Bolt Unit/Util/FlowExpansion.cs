6103c3c1fc2e5423ee9561a7f91bf065fef6728c
Icarus <1375400884@qq.com>
2018-08-21 23:22:23 +0800

    !T 修改了文件夹名字

---
+++ UnityGameFramework/Bolt Unit/Util/FlowExpansion.cs
@@ -0,0 +1,33 @@
+﻿//创建者:Icarus
+//手动滑稽,滑稽脸
+//ヾ(•ω•`)o
+//2018年08月21日-10:14
+//Icarus.UnityGameFramework.Bolt
+
+using Bolt;
+using Icarus.GameFramework;
+
+namespace Icarus.UnityGameFramework.Bolt.Util
+{
+    public static class FlowExpansion
+    {
+        public static void EnterControl(this Flow flow, ControlOutput control)
+        {
+            if (flow != null)
+            {
+                if (control != null)
+                {
+                    flow.Invoke(control);
+                }
+                else
+                {
+                    Log.Error("Control is null, Enter Control Failure!");
+                }
+            }
+            else
+            {
+                Log.Error("Flow is null, Enter Control Failure!");
+            }
+        }
+    }
+}
