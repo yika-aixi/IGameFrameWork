a244cb5669f41ed1125b800a16063b8e6c630baa
Icarus <1375400884@qq.com>
2018-08-25 03:02:03 +0800

    !W 新增了2个Flow扩展函数

--- UnityGameFramework/Bolt Unit/Util/FlowExpansion.cs
+++ UnityGameFramework/Bolt Unit/Util/FlowExpansion.cs
@@ -6,11 +6,13 @@
 
 using Bolt;
 using Icarus.GameFramework;
+using Ludiq;
 
 namespace Icarus.UnityGameFramework.Bolt.Util
 {
     public static class FlowExpansion
     {
+
         public static void EnterControl(this Flow flow, ControlOutput control)
         {
             if (flow != null)
@@ -28,6 +30,40 @@ namespace Icarus.UnityGameFramework.Bolt.Util
             {
                 Log.Error("Flow is null, Enter Control Failure!");
             }
+        }
+
+        public static void EnterControlAndDisplay(this Flow flow, ControlOutput control)
+        {
+            flow.EnterControl(control);
+            flow.Dispose();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="flow"></param>
+        /// <exception cref="GameFrameworkException">Flow  或 GraphStack 是空的</exception>
+        /// <returns></returns>
+        public static Flow GetNewFlow(this Flow flow)
+        {
+            if (flow == null)
+            {
+                throw new GameFrameworkException("Flow is null, Get New Flow Failure!");
+            }
+
+            if (flow.stack == null)
+            {
+                throw new GameFrameworkException("Flow of GraphStack is null, Get New Flow Failure!");
+            }
+
+            return GetFlow(flow.stack.AsReference());
+        }
+
+
+        public static Flow GetFlow(GraphReference reference)
+        {
+            return Flow.New(reference);
         }
     }
 }
