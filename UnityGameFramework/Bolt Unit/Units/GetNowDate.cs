e540191d5fbc65f6e2b301f36b223be2626b5411
Icarus <1375400884@qq.com>
2018-08-30 05:13:48 +0800

    !A 新增了获取当前时间和UGUI Text组件内容追加的Unit

---
+++ UnityGameFramework/Bolt Unit/Units/GetNowDate.cs
@@ -0,0 +1,46 @@
+﻿//创建者:Icarus
+//手动滑稽,滑稽脸
+//ヾ(•ω•`)o
+//2018年08月30日-01:35
+//Icarus.UnityGameFramework.Bolt
+
+using System;
+using Bolt;
+using Icarus.GameFramework;
+using Ludiq;
+
+namespace Icarus.UnityGameFramework.Bolt.Units
+{
+    [UnitCategory("Icarus/Util")]
+    [UnitTitle("Get Now Date Time")]
+    [UnitSubtitle("获取当前本地时间")]
+    public class GetNowDate : GameFrameWorkBaseUnit
+    {
+        [Serialize]
+        [Inspectable, InspectorLabel("输出格式:")]
+        public string _format = "yyyy/MM/dd HH:mm:ss";
+
+        [DoNotSerialize]
+        [PortLabel("Result")]
+        public ValueOutput _result;
+
+        protected override void Definition()
+        {
+            base.Definition();
+            _result = ValueOutput<string>(nameof(_result));
+            Assignment(_enter, _result);
+        }
+
+        protected override ControlOutput Enter(Flow flow)
+        {
+            var date = DateTime.Now;
+
+            var dateStr = date.ToString(_format);
+
+            flow.SetValue(_result, dateStr);
+
+
+            return _exit;
+        }
+    }
+}
---
+++ UnityGameFramework/Bolt Unit/Units/UGUI/TextAppend.cs
@@ -0,0 +1,62 @@
+﻿//创建者:Icarus
+//手动滑稽,滑稽脸
+//ヾ(•ω•`)o
+//2018年08月30日-01:19
+//Icarus.UnityGameFramework.Bolt
+
+using System.Text;
+using Bolt;
+using Ludiq;
+using UnityEngine.UI;
+
+namespace Icarus.UnityGameFramework.Bolt.Units.UGUI
+{
+    [UnitCategory("Icarus/Util/UGUI")]
+    [UnitTitle("Text Content Append")]
+    [UnitSubtitle("Text组件内容追加")]
+    public class TextAppend:GameFrameWorkBaseUnit
+    {
+
+        [Serialize]
+        [Inspectable, UnitHeaderInspectable("换行追加")]
+        public bool LineAppend;
+
+        [DoNotSerialize]
+        [PortLabel("Text")]
+        public ValueInput _textCom;
+
+        [DoNotSerialize]
+        [PortLabel("Content")]
+        public ValueInput _content;
+
+        protected override void Definition()
+        {
+            base.Definition();
+            _textCom = ValueInput<Text>(nameof(_textCom));
+            _content = ValueInput<object>(nameof(_content));
+
+            Requirement(_textCom,_enter);
+            Requirement(_content, _enter);
+        }
+
+        protected override ControlOutput Enter(Flow flow)
+        {
+            var text = flow.GetValue<Text>(_textCom);
+            var content = flow.GetValue<object>(_content);
+            StringBuilder sb = new StringBuilder(text.text);
+
+            if (LineAppend)
+            {
+                sb.AppendLine(content.ToString());
+            }
+            else
+            {
+                sb.Append(content);
+            }
+
+            text.text = sb.ToString();
+
+            return _exit;
+        }
+    }
+}
