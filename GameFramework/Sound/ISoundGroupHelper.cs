1aa05d1384ef11bca26e4b932e2a613c535f4e29
Icarus <1375400884@qq.com>
2018-08-30 21:44:58 +0800

    !A 新增了Sound模块

---
+++ GameFramework/Sound/ISoundGroupHelper.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------
+// Game Framework v3.x
+// Copyright © 2013-2018 Jiang Yin. All rights reserved.
+// Homepage: http://gameframework.cn/
+// Feedback: mailto:jiangyin@gameframework.cn
+//------------------------------------------------------------
+
+namespace Icarus.GameFramework.Sound
+{
+    /// <summary>
+    /// 声音组辅助器接口。
+    /// </summary>
+    public interface ISoundGroupHelper
+    {
+
+    }
+}
